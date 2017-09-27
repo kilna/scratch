0db5a3e3baec43dd291d932a9fb66d9a94a4f9ab
Kilna <kilna@kilna.com>
2017-09-27 16:56:28 -0500

    -

--- bash/make_docker_machine.sh
+++ bash/make_docker_machine.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-docker-machine create -d virtualbox --virtualbox-disk-size "30000" --virtualbox-memory 4096 --virtualbox-cpu-count 3 default
-'/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' controlvm default natpf1 tcp-port8080,tcp,,8080,,8080
-'/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' controlvm default natpf1 tcp-port443,tcp,,443,,443
-'/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' controlvm default natpf1 tcp-port5432,tcp,,5432,,5432
-'/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' controlvm default natpf1 tcp-port3306,tcp,,3306,,3306
+make_docker_machine() {
 
+  set -e
+
+  local machine_name="default"
+  local ports=(80 8000 8080 443 3306 5432 5000)
+  local adapters=(natpf1)
+
+  while true; do
+    [[ $(docker-machine rm --force "${machine_name}" 2>&1 | grep -F 'Host does not exist' ) != '' ]] && break
+    sleep 1
+  done
+
+  docker-machine create -d virtualbox --virtualbox-disk-size "30000" \
+      --virtualbox-memory 4096 --virtualbox-cpu-count 3 "${machine_name}"
+
+  for port in "${ports[@]}"; do
+    for adapter in "${adapters[@]}"; do
+      /c/Program\ Files/Oracle/VirtualBox/VBoxManage.exe controlvm "${machine_name}" "${adapter}" \
+          "port-${port},tcp,,${port},,${port}"  
+    done
+  done
+
+  eval `docker-machine env`
+  
+  set +e
+}
+
+make_docker_machine
