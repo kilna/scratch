a482b112724558ec6fb230dd07c41b92c0b493a4
Kilna <kilna@kilna.com>
2017-12-20 15:24:26 -0600

    Update options.sh

--- bash/options.sh
+++ bash/options.sh
@@ -22,26 +22,30 @@ scalar="default" # Single parameter (only one value, set by --scalar val )
 array=()         # Multiple parameter (appears more than once, added to array
                  #    set by passing --array val1 --array val2 )
 declare -A hash  # Hash parameter (appears more than once, associative array
-                 #   set by passing --hash key1=val1 --hash --key2=val2  )
+                 #   set by passing --hash key1=val1 --hash --key2=val2
+                 #   bash >= 4.0 only)
 
-# Load $@ into $opt array, changing --foo=bar options into separate --foo bar
-addopt(){ [[ "$1" == --*=* ]] && opt+=("${1%%=*}" "${1#*=}") || opt+=("$1"); }
-opt=(); for option in "$@"; do addopt "$option"; done
-optshift(){ opt=("${opt[@]:1}"); } # Shift $opt array 1 element left
-option(){ echo "${opt[0]}"; } # Current option (index 0 in $opt array)
+# Load $@ into $opt array, splitting --foo=bar options into separate --foo bar
+# and splitting bundled -abc into separate -a -b -c
+opt=(); for o in "$@"; do [[ "$o" == --*=* ]] && \
+opt+=("${1%%=*}" "${1#*=}") || [[ "$o" =~ ^-([^-]*)$ && "${BASH_REMATCH[1]}" \
+=~ ${BASH_REMATCH[1]//?/(.)} ]] && opt+=($(for x in "${BASH_REMATCH[@]:1}"; \
+do echo -n "-$x "; done)) || opt+=("$o"); done;
+optshift () { opt=("${opt[@]:1}"); }; # Shift $opt array 1 element left
+option () { echo "${opt[0]}"; }; # Current option (index 0 in $opt array)
 # Set key to value in hash based on a key=val string, remove if bash < 4.0
-hashset(){ eval "$1['${2%%=*}']='${2#*=}'"; }
+hashset () { eval "$1['${2%%=*}']='${2#*=}'"; };
 
 while true; do case "$(option)" in
 
   -s|--scalar) optshift; scalar="$(option)"; optshift ;;
   -a|--array)  optshift; array+=("$(option)"); optshift ;;
-  -h|--hash)   optshift; hashset hash "$(option)"; optshift ;; # bash > 4.0
+  -h|--hash)   optshift; hashset hash "$(option)"; optshift ;; # bash >= 4.0
   +b|--true)   optshift; bool=1 ;;
   -b|--false)  optshift; bool=0 ;;
-  --)          optshift; break ;; # Explicit end of options - nix if unneeded
-  -?*)         echo "Unknown option: $(option)" 1>&2; exit 1 ;; 
-  *)           break ;; # Implicit end of options - use exit 1 here if invalid
+  --)          optshift; break ;; # Explicit end of options, nix if unneeded
+  -?*)         echo "Unknown option: $(option)" 1>&2; exit 1 ;;
+  *)           break ;; # Implicit end of opts, exit 1 here to whitelist opts
 
 esac; done
 
@@ -49,7 +53,13 @@ esac; done
 
 echo "bool => $bool"
 echo "scalar => $scalar"
-for elem in "${array[@]}"; do echo "array => $elem"; done
-for key in "${!hash[@]}"; do echo "hash => $key = ${hash[$key]}"; done
+for elem in "${array[@]}"; do
+  echo "array => $elem"
+done
+for key in "${!hash[@]}"; do
+  echo "hash => $key = ${hash[$key]}"
+done
 # At this point $opt array only has remaining non-option entries from $@
-for option in "${opt[@]}"; do echo "opt => $option"; done
+for option in "${opt[@]}"; do
+  echo "opt => $option"
+done
