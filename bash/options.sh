fd2116c936ac51bc1fa374f588865a916721f33c
Kilna <kilna@kilna.com>
2017-12-20 23:19:28 -0600

    .

--- bash/options.sh
+++ bash/options.sh
@@ -12,8 +12,8 @@
 #  * --opt key=val (and --opt=key=val) for setting associative array pairs
 #      using very easy to read syntax (bash >= 4.0 only)
 #  * Support for non-dash options such as +x instead of -x
-#  * Pass-through remaining unrecognized post-options as an array ($opt)
-#  * Perservation of original $@ array or reset $@ to post-options
+#  * Pass-through remaining unrecognized command line args as array ($arg)
+#  * Perservation of original $@ array or reset $@ to remaining args
 #
 # Copyright Kilna - Released under Creative Commons CC-BY Atribution license
 
@@ -26,42 +26,29 @@ declare -A hash  # Hash parameter (appears more than once, associative array
                  #   set by passing --hash key1=val1 --hash --key2=val2
                  #   bash >= 4.0 only)
 
-# Load $@ into $opt array, splitting --foo=bar options into separate --foo bar
-# and splitting bundled -abc into separate -a -b -c
-opt=();
-for o in "$@"; do
-  [[ "$o" == --*=* ]] \
-    && opt+=("${1%%=*}" "${1#*=}") \
-    || [[ "$o" =~ ^-([^-]*)$ && "${BASH_REMATCH[1]}" =~ ${BASH_REMATCH[1]//?/(.)} ]] \
-      && opt+=($(for x in "${BASH_REMATCH[@]:1}"; do echo -n "-$x "; done)) \
-      || opt+=("$o")
-done
+args=(); for a in "$@"; do [[ "$a" == --*=* ]] && args+=("${a%%=*}" "${a#*=}") \
+|| [[ "$a" =~ ^-([^-]*)$ && "${BASH_REMATCH[1]}" =~ ${BASH_REMATCH[1]//?/(.)} \
+]] && args+=($(for x in "${BASH_REMATCH[@]:1}"; do echo -n "-$x "; done)) \
+|| args+=("$a"); done; arrayopt(){ nextarg; eval "$1+=('$(arg)')"; nextarg; }; 
+flagopt(){ nextarg; eval "$1=${2:-1}"; }; nextarg(){ args=("${args[@]:1}"); };
+arg(){ echo "${args[0]}"; }; opt(){ nextarg; eval "$1='$(arg)'"; nextarg; };
+hashopt(){ nextarg; eval "$1['${args[0]%%=*}']='${args[0]#*=}'"; nextarg; };
+argsleft(){ (( "${#args[@]}" > 0 )); };
 
-for option in "${opt[@]}"; do
-  echo "opt => $option"
-done
+while argsleft; do case "$(arg)" in
 
-optshift () { opt=("${opt[@]:1}"); }; # Shift $opt array 1 element left
-option () { echo -n "${opt[0]}"; }; # Current option (index 0 in $opt array)
-optval () { optshift; echo -n "${opt[0]}"; optshift; }; # Process options having values
-moreopts () { (( "${#opt}" >= 1 )); }; # Are there remaining options?
-# Set key to value in hash based on a key=val string, remove if bash < 4.0
-hashopt () { local kv="$(optval)"; eval "$1['${kv%%=*}']='${kv#*=}'"; };
-
-while moreopts; do case "$(option)" in
-
-  -s|--scalar) scalar="$(optval)" ;;
-  -a|--array)  array+=("$(optval)") ;;
-  -h|--hash)   hashopt hash ;; # bash >= 4.0 only
-  +b|--true)   bool=1; optshift ;;
-  -b|--false)  bool=0; optshift ;;
-  --)          optshift; break ;; # Explicit end of options, nix if unneeded
-  -?*)         echo "Unknown option: $(option)" 1>&2; exit 1 ;;
-  *)           break ;; # Implicit end of opts, exit 1 here to whitelist opts
+  -s|--scalar) opt 'scalar' ;;     # Sets $scalar to provided value
+  -a|--array)  arrayopt 'array' ;; # Adds value as an element in $array
+  -h|--hash)   hashopt 'hash' ;;   # Adds key value to $hash (bash >= 4.0 only)
+  +b|--true)   flagopt 'bool' ;;   # Sets $bool to 1
+  -b|--false)  flagopt 'bool' 0 ;; # Sets $bool to 0
+  --)          nextarg; break ;;   # Explicit end of opts, nix if unneeded
+  -?*)         echo "Unknown option: $(arg)" 1>&2; exit 1 ;;
+  *)           break ;; # Implicit opts end, exit 1 for only whitelisted opts
 
 esac; done
 
-#set -- "${opt[@]}" # Uncomment to reset the $@ array to $opt (remainder)
+#set -- "${arg[@]}" # Uncomment to reset the $@ array to $arg (remaining opts)
 
 echo "bool => $bool"
 echo "scalar => $scalar"
@@ -71,7 +58,8 @@ done
 for key in "${!hash[@]}"; do
   echo "hash => $key = ${hash[$key]}"
 done
-# At this point $opt array only has remaining non-option entries from $@
-for option in "${opt[@]}"; do
-  echo "opt => $option"
+# At this point $arg array only has remaining non-option entries from $@
+for arg in "${args[@]}"; do
+  echo "arg => $arg"
 done
+
