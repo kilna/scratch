ec9d22fd20ef8a619888b4d9445ebcafa4b63835
Kilna <kilna@kilna.com>
2018-11-14 15:43:20 -0800

    Update opt.sh

--- bash/opt.sh
+++ bash/opt.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Echoes the value of an option, returns a non-zero exit if a shift is needed
-# Valid opt styles:   -a val   -aval   -a=val   --arg val   --arg=val
-# Options cannot be bundled
+# A pure-bash huffman-coded option handler
+
+# Long/commented version:
+
+# Echoes the value of the next option in "$@", fails if an additional shift is needed
 #
+# Valid opt styles:   -a val   -aval   -a=val   --arg val   --arg=val
+#
+# Options cannot be bundled
+
 # Typically used like:
 #
-# timeout=60
-# while [[ $# -gt 0 ]]; do case "$1" in
-#   -t*|--timeout*)  timeout="$(opt "$@")" || shift;;
-#   *)               die "Unknown option: $1";;
-# esac; shift; done
-#
+#    timeout=60
+#    while [[ $# -gt 0 ]]; do case "$1" in
+#        -t*|--timeout*)  timeout="$(opt "$@")" || shift;;
+#        *)               echo "Unknown option: $1" >&2; exit 1;;
+#    esac; shift; done
+
+# Caveat:
+# Default pattern of using a case pattern of --optname*) means that --optnameblah would
+# match and be processed in the same way. As a result, if you need to parse any option
+# that is a substring of another, different option, you should process long options
+# first in your case statement
+
 opt() {
   if [[ "${#1}" -gt 2 && "${1:1:1}" != '-' && "${1:2:1}" != '=' ]]; then
     # Looks like -aval (but not -a=val), return character 2 onward
@@ -29,10 +41,15 @@ opt() {
   fi
 }
 
+
 # Compact version:
 
-# Echoes the value of an option, returns a non-zero exit if a shift is needed
+# Gets the value of the next option in $@, fails if an additional shift is needed
+# Details at https://github.com/kilna/scratch/blob/master/bash/opt.sh
 opt() {
   if [[ "${#1}" -gt 2 && "${1:1:1}" != '-' && "${1:2:1}" != '=' ]]; then echo "${1:2}"
   elif [[ "$1" == *'='* ]]; then echo "${1#*=}"; else echo "$2"; return 1; fi
 }
+
+
+# MIT license, but it'd be nice if you include the link to here
